4aab728fe8485d0da353a0218c8c481f4cd56b4d
shettyadithi <97408508+shettyadithi@users.noreply.github.com>
2022-04-11 13:21:40 -0400

    Update video.js

--- js/video.js
+++ js/video.js
@@ -33,7 +33,7 @@ window.addEventListener("load", function() {
 
 	/* ---------------- Slow Down ---------------- */
 	document.querySelector("#slower").addEventListener("click", function() {
-    video.playbackRate -= (0.05 * video.playbackRate);
+    video.playbackRate -= 0.95;
 	console.log("New speed is  " + video.playbackRate);
 	});
 
